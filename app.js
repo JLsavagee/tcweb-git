849ab2cdad0c1dbac19066ea9af5d3b5db983a13
Jan-Luca Maiwaldt <janluca@maiwaldt.de>
2024-05-05 00:04:24 +0200

    script changes

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, './frontend')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+});
+
 app.listen(3000, () => {
   console.log('Server läuft auf Port 3000');
 });
