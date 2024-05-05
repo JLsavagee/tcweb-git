8a8ee2af1df917a1b034d0d47c8cf383e77e90ec
Jan-Luca Maiwaldt <janluca@maiwaldt.de>
2024-05-05 23:06:43 +0200

    reverse app.js changes and take out backend of starting command

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,14 @@
+// backend/app.js
+
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors'); // Include CORS
 const uploadRoutes = require('./routes/upload');
-const findPlayer = require('./routes/find_player');
+const findPlayer = require('./routes/find_player')
 const startWatching = require('./file_watcher');
 
 const app = express();
 
-// Configure CORS
-app.use(cors({
-  origin: ['http://team-cards.de', 'https://team-cards.de'],
-  methods: ['GET', 'POST'], // Specify allowed methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
-}));
-
+app.use(cors()); // Apply CORS middleware to enable cross-origin requests
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -22,14 +18,8 @@ app.use('/api', findPlayer);
 
 const PORT = process.env.PORT || 5001;
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-
 startWatching();
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
