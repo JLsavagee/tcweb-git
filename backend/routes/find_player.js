75eb53b0a230f1098659d27841925dc60424c2b4
Jan-Luca Maiwaldt <janluca@maiwaldt.de>
2024-05-05 22:52:56 +0200

    comit for what

--- backend/routes/find_player.js
+++
@@ -1,9 +0,0 @@
-// backend/routes/find_player.js
-
-const express = require('express');
-const router = express.Router();
-const { submitAnswer } = require('../controllers/PlayerController');
-
-router.post('/submit-answer', submitAnswer);
-
-module.exports = router;
