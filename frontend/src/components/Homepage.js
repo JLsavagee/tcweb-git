e30fd3c65a604f29fccfbe809189e2f6445ccf3e
Jan-Luca Maiwaldt <janluca@maiwaldt.de>
2024-05-05 19:59:57 +0200

    improved Homepage Design

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -34,23 +34,27 @@ function Homepage() {
 
       <div className="section3">
         <div className='section3-headline'>
-          <h1>So funktioniert's</h1>
+          <h2>So funktioniert's</h2>
         </div>
         <div className='box-1'>
           <FontAwesomeIcon icon={faIdCard} className='icon'/>
           <h1>Name, Foto, Position</h1>
-          <h2>Den Rest machen Wir!</h2>
           <p>Über unsere Upload Funktion kannst du ganz einfach die Daten des Spielers angeben.</p>
+          <p>Minimaler Aufwand für alle beteiligten!</p>
         </div>
         <div className='box-2'>
           <FontAwesomeIcon icon={faStar} className='icon'/>
           <h1>Drucken, Fertig, Los!</h1>
           <p>Die kleinen Stars werden im hochwertigen Sammelkarten Format aussehen wie ihre Idole! </p>
+          <p>Möchtest du individuelle Karten auf deinem Event austeilen?</p>
+          <button>Für Organisatoren</button>
         </div>
         <div className='box-3'>
           <FontAwesomeIcon icon={faFaceLaughBeam} className='icon'/>
           <h1>Fußball, Freude, kleine Stars!</h1>
           <p>Seien Sie der Vorreiter mit individuellen Fußballkarten als Teil Ihres Sponsoren-Pakets.</p>
+          <p>Sind Sie interessiert daran Sammelkarten als Sponsorenleistung anzubieten?</p>
+          <button>Für Sponsoren</button>
         </div>
       </div>
 
