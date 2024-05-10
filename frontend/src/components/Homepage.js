efa8c01707b8d4417841479f95f919c3a264ad79
Jan-Luca Maiwaldt <janluca@maiwaldt.de>
2024-05-11 01:30:10 +0200

    mobile opti organisatoren

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -51,20 +51,20 @@ function Homepage() {
             <h2>So funktioniert's</h2>
           </div>
           <div className='hp-box-1'>
-            <FontAwesomeIcon icon={faIdCard} className='icon'/>
+            <FontAwesomeIcon icon={faIdCard} className='hp-icon'/>
             <h1>Name, Foto, Position</h1>
             <p>Über unsere Upload Funktion kannst du ganz einfach die Daten des Spielers angeben.</p>
             <p>Minimaler Aufwand für alle beteiligten!</p>
           </div>
           <div className='hp-box-2'>
-            <FontAwesomeIcon icon={faStar} className='icon'/>
+            <FontAwesomeIcon icon={faStar} className='hp-icon'/>
             <h1>Drucken, Fertig, Los!</h1>
             <p>Die kleinen Stars werden im hochwertigen Sammelkarten Format aussehen wie ihre Idole! </p>
             <p>Möchtest du individuelle Karten auf deinem Event austeilen?</p>
             <button onClick={handleClick}>Für Organisatoren</button>
           </div>
           <div className='hp-box-3'>
-            <FontAwesomeIcon icon={faFaceLaughBeam} className='icon'/>
+            <FontAwesomeIcon icon={faFaceLaughBeam} className='hp-icon'/>
             <h1>Fußball, Freude, kleine Stars!</h1>
             <p>Seien Sie der Vorreiter mit individuellen Fußballkarten als Teil Ihres Sponsoren-Pakets.</p>
             <p>Sind Sie interessiert daran Sammelkarten als Sponsorenleistung anzubieten?</p>
