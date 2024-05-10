e1d5cae0f25d8c2e0399369fe151b11bc38d4e9d
Jan-Luca Maiwaldt <janluca@maiwaldt.de>
2024-05-11 00:27:29 +0200

    mobile opti Homepage

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -23,48 +23,53 @@ function Homepage() {
   return (
     <div>
       <Navbar />
-
-      <div className="section1">
-        <div className="section1-text">
-            <h1>Dein Fußball Erlebnis wird</h1>
-            <h2>ERINNERUNG!</h2>
-            <h3>Individuelle Sammelkarten für deine Events</h3>
-        </div>
-        <div className='section1-image'>
-          <img src={Image4} alt="Bild" className="section2-image" /> 
+      <div className='container'>
+        <div className="hp-section1">
+          <div className="hp-section1-text">
+              <h1>Dein Fußball Erlebnis wird</h1>
+              <h2>ERINNERUNG!</h2>
+              <h3>Individuelle Sammelkarten für deine Events</h3>
+          </div>
+          <div className='hp-section1-image'>
+            <img src={Image4} alt="Bild" /> 
+          </div>
         </div>
       </div>
-
-      <div className='example-pictures'>
-          <img src={Image4} alt="Bild" className="section2-image" /> 
-          <img src={Image4} alt="Bild" className="section2-image" /> 
-          <img src={Image4} alt="Bild" className="section2-image" /> 
-          <img src={Image4} alt="Bild" className="section2-image" /> 
+      <div className='container'>
+        <div className='hp-section2'>
+        <div className='hp-example-pictures'>
+            <img src={Image4} alt="Bild"  /> 
+            <img src={Image4} alt="Bild"  /> 
+            <img src={Image4} alt="Bild"  /> 
+            <img src={Image4} alt="Bild"  /> 
+        </div>
+        </div>
       </div>
-
-      <div className="section3">
-        <div className='section3-headline'>
-          <h2>So funktioniert's</h2>
-        </div>
-        <div className='box-1'>
-          <FontAwesomeIcon icon={faIdCard} className='icon'/>
-          <h1>Name, Foto, Position</h1>
-          <p>Über unsere Upload Funktion kannst du ganz einfach die Daten des Spielers angeben.</p>
-          <p>Minimaler Aufwand für alle beteiligten!</p>
-        </div>
-        <div className='box-2'>
-          <FontAwesomeIcon icon={faStar} className='icon'/>
-          <h1>Drucken, Fertig, Los!</h1>
-          <p>Die kleinen Stars werden im hochwertigen Sammelkarten Format aussehen wie ihre Idole! </p>
-          <p>Möchtest du individuelle Karten auf deinem Event austeilen?</p>
-          <button onClick={handleClick}>Für Organisatoren</button>
-        </div>
-        <div className='box-3'>
-          <FontAwesomeIcon icon={faFaceLaughBeam} className='icon'/>
-          <h1>Fußball, Freude, kleine Stars!</h1>
-          <p>Seien Sie der Vorreiter mit individuellen Fußballkarten als Teil Ihres Sponsoren-Pakets.</p>
-          <p>Sind Sie interessiert daran Sammelkarten als Sponsorenleistung anzubieten?</p>
-          <button onClick={handleClick2}>Für Sponsoren</button>
+      <div className='container'>
+        <div className="hp-section3">
+          <div className='hp-section3-headline'>
+            <h2>So funktioniert's</h2>
+          </div>
+          <div className='hp-box-1'>
+            <FontAwesomeIcon icon={faIdCard} className='icon'/>
+            <h1>Name, Foto, Position</h1>
+            <p>Über unsere Upload Funktion kannst du ganz einfach die Daten des Spielers angeben.</p>
+            <p>Minimaler Aufwand für alle beteiligten!</p>
+          </div>
+          <div className='hp-box-2'>
+            <FontAwesomeIcon icon={faStar} className='icon'/>
+            <h1>Drucken, Fertig, Los!</h1>
+            <p>Die kleinen Stars werden im hochwertigen Sammelkarten Format aussehen wie ihre Idole! </p>
+            <p>Möchtest du individuelle Karten auf deinem Event austeilen?</p>
+            <button onClick={handleClick}>Für Organisatoren</button>
+          </div>
+          <div className='hp-box-3'>
+            <FontAwesomeIcon icon={faFaceLaughBeam} className='icon'/>
+            <h1>Fußball, Freude, kleine Stars!</h1>
+            <p>Seien Sie der Vorreiter mit individuellen Fußballkarten als Teil Ihres Sponsoren-Pakets.</p>
+            <p>Sind Sie interessiert daran Sammelkarten als Sponsorenleistung anzubieten?</p>
+            <button onClick={handleClick2}>Für Sponsoren</button>
+          </div>
         </div>
       </div>
 
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,27 +4,29 @@ import '../css/Navbar.css'; // Pfad zu deiner CSS-Datei anpassen, falls nötig
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-       <span className="navbar-brand">Team-cards.de</span>
-        <NavLink to="/" className="nav-link">
-          Home
-        </NavLink>
-        {/* <NavLink to="/spieler-quiz" className="nav-link">
-          Spieler-Quiz
-        </NavLink> */}
-        <NavLink to="/organisatoren" className="nav-link">
-          Für Organisatoren
-        </NavLink>
-        <NavLink to="/sponsoren" className="nav-link">
-          Für Sponsoren
-        </NavLink>
-        <NavLink to="/foto-upload" className="nav-link">
-          Foto Upload
-        </NavLink>
-        <NavLink to="/über-uns" className="nav-link">
-          Über Uns
-        </NavLink>
-    </nav>
+    <div className='container'>
+      <nav className="navbar">
+        <span className="navbar-brand">Team-cards.de</span>
+          <NavLink to="/" className="nav-link">
+            Home
+          </NavLink>
+          {/* <NavLink to="/spieler-quiz" className="nav-link">
+            Spieler-Quiz
+          </NavLink> */}
+          <NavLink to="/organisatoren" className="nav-link">
+            Für Organisatoren
+          </NavLink>
+          <NavLink to="/sponsoren" className="nav-link">
+            Für Sponsoren
+          </NavLink>
+          <NavLink to="/foto-upload" className="nav-link">
+            Foto Upload
+          </NavLink>
+          <NavLink to="/über-uns" className="nav-link">
+            Über Uns
+          </NavLink>
+      </nav>
+    </div>
   );
 };
 export default Navbar;
