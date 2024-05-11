1300c69b7ec7fbc8d90305150d8b2c9264c4034f
Jan-Luca Maiwaldt <janluca@maiwaldt.de>
2024-05-12 00:00:56 +0200

    foldable navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import {useRef} from "react";
+import {FaBars} from "react-icons/fa";
 import '../css/Navbar.css'; // Pfad zu deiner CSS-Datei anpassen, falls nötig
 
-const Navbar = () => {
+function Navbar()  {
+    const navRef = useRef();
+
+    const showNavbar = () => {
+      navRef.current.classList.toggle("responsive_nav")
+    }
   return (
-    <div className='container'>
-      <nav className="navbar">
-        <span className="navbar-brand">Team-cards.de</span>
+  <div className='container'>
+    <span className="navbar-brand">Team-cards.de</span>
+      <nav ref={navRef} className="navbar">
           <NavLink to="/" className="nav-link">
             Home
           </NavLink>
@@ -26,6 +33,9 @@ const Navbar = () => {
             Über Uns
           </NavLink>
       </nav>
+      <button className='nav-btn' onClick={showNavbar}>
+        <FaBars />
+      </button>
     </div>
   );
 };
